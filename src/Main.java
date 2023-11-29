137bdb8bbcebc0d2b7c429a5e60150871d4c63e3
Stefano <stegallo1990@gmail.comgit config -lgit config --global user.email stegallo1990@gmail.com>
2023-11-29 19:26:24 +0100

    aggiunta classe Auto

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        Auto auto = new Auto(1100,"CC345ZZ","Citroen","C2");
+        Auto auto2 = new Auto(1500,"CC345ZZ","Citroen","C2");
+
+     System.out.println("Auto: " + auto);
+     auto.stampaCilindrata();
+     auto2.stampaCilindrata();
+
+
+
+
+    }
+}
